4ab42361600a6318dd7c8f75df12a8521ef83e11
yeisonmarroquin <andresmarroquin887@gmail.com>
2023-05-06 17:24:09 -0500

    Portafolio

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Loading from './components/Loading'
+import Portafolio from './page/Portafolio';
+import { Route, Routes } from 'react-router-dom';
+import Proyects from './page/Proyects';
+
+function App() {
+  const [Isloanding, setIsloanding] = useState(true)
+  
+    setTimeout(() => {
+      setIsloanding(false);
+    }, 3050);
+ 
+  return (
+    <div className='App'>
+      {
+        Isloanding 
+        ?<Loading Isloanding={Isloanding}/>
+        :(
+          <Routes>
+            <Route path='/' element={<Portafolio/>}/>
+            <Route path='/proyects' element={<Proyects/>}/>
+          </Routes>
+        )
+      }
+    </div>
+  )
+}
+
+export default App
+
