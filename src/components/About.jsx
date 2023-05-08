871e58be2e883c9af6503ef68314bba339d0ba4a
yeisonmarroquin <andresmarroquin887@gmail.com>
2023-05-08 15:00:02 -0500

    Portafolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import './style/About.css'
 const About = () => {
   return (
     <div>
       
-      <section id="About" className="Portafolio_">
-        <nav className="Portafolio_">
-          <h1 className="Portafolio_">
+<section id="About" className="About">
+        <nav className="About_history">
+          <h1 className="About_tittle">
             Hello, here I leave you a little of my history as a programmer...
           </h1>
-          <p className="Portafolio_">
+      <p className="About_his">
             Throughout my career as a programmer I have been through many things
             good, I have learned a lot from this profession and I feel very
             comfortable To do it every day, I am a very young boy I am only 18
@@ -17,22 +17,23 @@ const About = () => {
             goal is to program Any project to be carried out in the world of
             programming.
           </p>
-        </nav>
-        <nav className="Portafolio_">
+          <br />
+        <nav className="About_image">
           <img
-            className="Portafolio_"
+          className="About_img"
             src="./image/trabajo.jpg"
             alt="trabajo"
           />
         </nav>
-        <nav className="Portafolio_">
-          <p className="Portafolio_">
+        <br />
+          <p className="About_his">
             Some time ago I did not know anything about programming and today my
             Progress has been impressive and in a very short time, I am now a
             Frot-end programmer and I feel ready to learn as much as I can From
             now on as I have been doing so far.
           </p>
-        </nav>
+          
+          </nav>
       </section>
     </div>
   )
