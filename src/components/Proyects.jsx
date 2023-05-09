85a6dce3c9724f67e9f8d2a1f3f323efa77eac88
yeisonmarroquin <andresmarroquin887@gmail.com>
2023-05-08 21:15:59 -0500

    Portafolio

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -21,50 +21,53 @@ const Proyects = () => {
       </nav>
 
       <nav  className='Proyects_proyect'>
-        <img  className='Proyects_' src="./image/Riky.jpg" alt="" />
-        <nav  className='Proyects_'>
-          <h1  className='Proyects_'>Project riky and morti</h1>
-          <p  className='Proyects_'>
+        <img  className='Proyects_img' src="./image/Riky.jpg" alt="" />
+        <nav  className='Proyects_info'>
+          <h1  className='Proyects_name'>Project riky and morti</h1>
+          <p  className='Proyects_text'>
             As in the previous project here in this we can see the famous
             characters of the saga of Riky and Morti, which is a very
             entertaining saga, because here you can find all its characters.
           </p>
-          <div  className='Proyects_'>
-            <h1  className='Proyects_'>More information</h1>
+          <div  className='Proyects_click'>
+            <h1  className='Proyects_ingresar'>More information</h1>
           </div>
         </nav>
       </nav>
 
       <nav  className='Proyects_proyect'>
-        <img  className='Proyects_' src="./image/crud.jpg" alt="" />
-        <nav  className='Proyects_'>
-          <h1  className='Proyects_'>Crud</h1>
-          <p  className='Proyects_'>
+        <img  className='Proyects_img' src="./image/crud.jpg" alt="" />
+        <nav  className='Proyects_info'>
+          <h1  className='Proyects_name'>Crud</h1>
+          <p  className='Proyects_text'>
             This project is a database in which you can make a record of a
             person and also edit or delete it, it is a great app try it you
             will like it
           </p>
-          <div  className='Proyects_'>
-            <h1  className='Proyects_'>More information</h1>
+          <div  className='Proyects_click'>
+            <h1  className='Proyects_ingresar'>More information</h1>
           </div>
         </nav>
       </nav>
 
       <nav  className='Proyects_proyect'>
-        <img  className='Proyects_' src="" alt="" />
-        <nav  className='Proyects_'>
-          <h1  className='Proyects_'>Weather App</h1>
-          <p  className='Proyects_'>
+        <img  className='Proyects_img' src="./image/wheather.jpg" alt="" />
+        <nav  className='Proyects_info'>
+          <h1  className='Proyects_name'>Weather App</h1>
+          <p  className='Proyects_text'>
             Here I have this app, which tells us the weather we have in the
             place where we are and we can also look for the weather of other
             places
           </p>
-          <div  className='Proyects_'>
-            <h1  className='Proyects_'>More information</h1>
+          <div  className='Proyects_click'>
+            <h1  className='Proyects_ingresar'>More information</h1>
           </div>
         </nav>
       </nav>
     </section>
+    <div className='proyects_todos'>
+      <h1>enter here for <br /> more information</h1>
+    </div>
   </section>
   )
 }
