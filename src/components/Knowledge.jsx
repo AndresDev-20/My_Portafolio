1bde8f89f2371d5ca22311de684bfb4f3e551530
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-10-03 17:23:42 -0500

    My_Portafolio

--- src/components/Knowledge.jsx
+++ src/components/Knowledge.jsx
@@ -14,51 +14,67 @@ const Knowledge = () => {
     <section className='languaje'>
       <a href="https://developer.mozilla.org/es/docs/Web/HTML">
           <nav className="Knowledge_languaje">
-        <img className="Knowledge_img" src="./image/html.png" alt="" />
+          <i className='html bx bxl-html5'></i>
         <h1 className="Knowledge_name">Html</h1>
       </nav>
       </a>
         <a href="https://developer.mozilla.org/es/docs/Web/CSS">
             <nav className="Knowledge_languaje">
-        <img className='Knowledge_img' src="./image/css.jpeg" alt="" />
+            <i className='bx Css bxl-css3'></i>
         <h1 className='Knowledge_name'>Css</h1>
       </nav>
         </a>
          <a href="https://developer.mozilla.org/es/docs/Web/JavaScript">
            <nav className='Knowledge_languaje'>
-        <img className='Knowledge_img' src="./image/js.jpg" alt="" />
-        <h1 className='Knowledge_name'>Js</h1>
+           <i className='bx js bxl-javascript' ></i>
+        <h1 className='Knowledge_name'>JavaScript</h1>
       </nav>
          </a>
           <a href="https://react.dev/learn">
               <nav className='Knowledge_languaje'>
-        <img  className="Knowledge_img" src="./image/react.jpg" alt="" />
+              <i className='bx react bxl-react' ></i>
         <h1 className='Knowledge_name'>React</h1>
       </nav>
           </a>
         <a href="https://docs.github.com/en/apps">
           <nav className='Knowledge_languaje'>
-        <img className='Knowledge_img' src="./image/git.jpg" alt="" />
+          <i className='bx git bxl-github'></i>
         <h1 className='Knowledge_name'>GitHub</h1>
       </nav>
         </a>
              <a href="https://redux.js.org/">
            <nav className='Knowledge_languaje'>
-        <img className='Knowledge_img' src="./image/redux.jpg" alt="" />
+           <i className='bx redux bxl-redux' ></i>
         <h1 className='Knowledge_name'>Redux</h1>
       </nav>
          </a>
          <a href="https://wiki.postgresql.org/wiki/Main_Page/es">
            <nav className='Knowledge_languaje'>
-        <img className='Knowledge_img' src="./image/SQL.png" alt="" />
+           <i className='bx post bxl-postgresql' ></i>
         <h1 className='Knowledge_name'>SQL</h1>
       </nav>
          </a>
          <a href="https://nodejs.org/es/docs">
            <nav className='Knowledge_languaje'>
-        <img className='Knowledge_img' src="./image/node.png" alt="" />
+           <i className='bx node bxl-nodejs'></i>
         <h1 className='Knowledge_name'>Node.Js</h1>
       </nav>
+      
+         </a>
+<a href="https://nodejs.org/es/docs">
+  <nav className='Knowledge_languaje'>
+    <i className='bx bxl-python python-icon'></i>
+    <h1 className='Knowledge_name'>Python</h1>
+  </nav>
+</a>
+
+
+        <a href="https://nodejs.org/es/docs">
+           <nav className='Knowledge_languaje'>
+           <i class='bx type bxl-typescript' ></i>
+        <h1 className='Knowledge_name'>TypeScript</h1>
+      </nav>
+      
          </a>
     </section>
     
