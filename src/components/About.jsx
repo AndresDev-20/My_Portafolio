1f03241539cf5c83459c82550447e04c9d7f6911
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-10-30 18:24:10 -0500

    My Portafoly

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,40 +2,24 @@ import React from "react";
 import "./style/About.css";
 const About = () => {
   return (
-    <div  id="About">
-      <section className="About">
-        <nav className="About_history">
-          <h1 className="About_tittle">
-           Algo sobre mi...
-          </h1>
-          <section className="About_historyy">
-            <p className="About_his">
-            Desarrollador de software apasionado
-con habilidades en resolución creativa de
-problemas y experiencia en proyectos
-colaborativos. Comprometido con el
-aprendizaje continuo y la adaptación
-rápida a nuevos entornos laborales. Listo
-para aportar mi pasión y conocimiento al
-equipo y enfrentar desafíos con
-determinación.
-<br />
-Experiencia en proyectos colaborativos,
-demostrando trabajo en equipo.
-Adaptable a nuevos entornos laborales y
-enfocado en el aprendizaje continuo.
-Habilidad para liderar equipos y
-solucionar problemas de manera rápida y
-efectiva.
-
-            </p>
-            <br />
-
-            <br />
-            <p className="About_his"></p>
-          </section>
-        </nav>
-      </section>
+    <div id="About" className="About">
+      <br /><br /><br /><br /><br /><br />
+      
+      <h1>Algo sobre mi...</h1>
+      <p>
+        Desarrollador de software apasionado con habilidades en resolución
+        creativa de problemas y experiencia en proyectos colaborativos.
+        Comprometido con el aprendizaje continuo y la adaptación rápida a nuevos
+        entornos laborales. Listo para aportar mi pasión y conocimiento al
+        equipo y enfrentar desafíos con determinación.
+      </p>
+      <br />
+      <p>
+        Experiencia en proyectos colaborativos, demostrando trabajo en equipo.
+        Adaptable a nuevos entornos laborales y enfocado en el aprendizaje
+        continuo. Habilidad para liderar equipos y solucionar problemas de
+        manera rápida y efectiva.
+      </p>
     </div>
   );
 };
--- src/components/Knowledge.jsx
+++ src/components/Knowledge.jsx
@@ -1,89 +1,81 @@
-import React from 'react'
-import './style/Knowledge.css'
-import Skills from './Skills'
+import React from "react";
+import "./style/Knowledge.css";
+import Skills from "./Skills";
 const Knowledge = () => {
   return (
-   
-    <section className="Knowledge" id='Knowledge'>
-    <h1 className="Knowledge_tittle">Conocimiento</h1>
-    <p  className='Knowledge_text'>
-    Estas son algunas tecnologias que he aprendido hasta el momento
-          </p>
-          <br />
-    <section className="Knowledge_knowledge">
-    <section className='languaje'>
-      <a href="https://developer.mozilla.org/es/docs/Web/HTML">
+    <section id="Knowledge">
+      <br /><br /><br /><br /><br />
+      <h1>Tecnologias</h1>
+      <p>
+        Estas son las tecnologías con las que trabajo actualmente. Aunque la
+        lista puede no ser extensa, mi enfoque constante en el aprendizaje me
+        impulsa a adquirir nuevas habilidades de manera continua.
+      </p>
+      <section className="Knowledge">
+        <a href="https://developer.mozilla.org/es/docs/Web/HTML">
           <nav className="Knowledge_languaje">
-          <i className='html bx bxl-html5'></i>
-        <h1 className="Knowledge_name">Html</h1>
-      </nav>
-      </a>
+            <i className="html bx bxl-html5"></i>
+            <h1 className="Knowledge_name">Html</h1>
+          </nav>
+        </a>
         <a href="https://developer.mozilla.org/es/docs/Web/CSS">
-            <nav className="Knowledge_languaje">
-            <i className='bx Css bxl-css3'></i>
-        <h1 className='Knowledge_name'>Css</h1>
-      </nav>
+          <nav className="Knowledge_languaje">
+            <i className="bx Css bxl-css3"></i>
+            <h1 className="Knowledge_name">Css</h1>
+          </nav>
         </a>
-         <a href="https://developer.mozilla.org/es/docs/Web/JavaScript">
-           <nav className='Knowledge_languaje'>
-           <i className='bx js bxl-javascript' ></i>
-        <h1 className='Knowledge_name'>JavaScript</h1>
-      </nav>
-         </a>
-          <a href="https://react.dev/learn">
-              <nav className='Knowledge_languaje'>
-              <i className='bx react bxl-react' ></i>
-        <h1 className='Knowledge_name'>React</h1>
-      </nav>
-          </a>
+        <a href="https://developer.mozilla.org/es/docs/Web/JavaScript">
+          <nav className="Knowledge_languaje">
+            <i className="bx js bxl-javascript"></i>
+            <h1 className="Knowledge_name">JavaScript</h1>
+          </nav>
+        </a>
+        <a href="https://react.dev/learn">
+          <nav className="Knowledge_languaje">
+            <i className="bx react bxl-react"></i>
+            <h1 className="Knowledge_name">React</h1>
+          </nav>
+        </a>
         <a href="https://docs.github.com/en/apps">
-          <nav className='Knowledge_languaje'>
-          <i className='bx git bxl-github'></i>
-        <h1 className='Knowledge_name'>GitHub</h1>
-      </nav>
+          <nav className="Knowledge_languaje">
+            <i id="githubb" className="bx git bxl-github"></i>
+            <h1 className="Knowledge_name">GitHub</h1>
+          </nav>
         </a>
-             <a href="https://redux.js.org/">
-           <nav className='Knowledge_languaje'>
-           <i className='bx redux bxl-redux' ></i>
-        <h1 className='Knowledge_name'>Redux</h1>
-      </nav>
-         </a>
-         <a href="https://wiki.postgresql.org/wiki/Main_Page/es">
-           <nav className='Knowledge_languaje'>
-           <i className='bx post bxl-postgresql' ></i>
-        <h1 className='Knowledge_name'>SQL</h1>
-      </nav>
-         </a>
-         <a href="https://nodejs.org/es/docs">
-           <nav className='Knowledge_languaje'>
-           <i className='bx node bxl-nodejs'></i>
-        <h1 className='Knowledge_name'>Node.Js</h1>
-      </nav>
-      
-         </a>
-<a href="https://nodejs.org/es/docs">
-  <nav className='Knowledge_languaje'>
-    <i className='bx bxl-python python-icon'></i>
-    <h1 className='Knowledge_name'>Python</h1>
-  </nav>
-</a>
-
+        <a href="https://redux.js.org/">
+          <nav className="Knowledge_languaje">
+            <i className="bx redux bxl-redux"></i>
+            <h1 className="Knowledge_name">Redux</h1>
+          </nav>
+        </a>
+        <a href="https://wiki.postgresql.org/wiki/Main_Page/es">
+          <nav className="Knowledge_languaje">
+            <i className="bx post bxl-postgresql"></i>
+            <h1 className="Knowledge_name">SQL</h1>
+          </nav>
+        </a>
+        <a href="https://nodejs.org/es/docs">
+          <nav className="Knowledge_languaje">
+            <i className="bx node bxl-nodejs"></i>
+            <h1 className="Knowledge_name">Node.Js</h1>
+          </nav>
+        </a>
+        <a href="https://nodejs.org/es/docs">
+          <nav className="Knowledge_languaje">
+            <i className="bx bxl-python python-icon"></i>
+            <h1 className="Knowledge_name">Python</h1>
+          </nav>
+        </a>
 
         <a href="https://nodejs.org/es/docs">
-           <nav className='Knowledge_languaje'>
-           <i class='bx type bxl-typescript' ></i>
-        <h1 className='Knowledge_name'>TypeScript</h1>
-      </nav>
-      
-         </a>
+          <nav className="Knowledge_languaje">
+            <i class="bx type bxl-typescript"></i>
+            <h1 className="Knowledge_name">TypeScript</h1>
+          </nav>
+        </a>
+      </section>
     </section>
-    
-  
-    </section>
-    
- 
-  </section>
-  )
-}
+  );
+};
 
-export default Knowledge
+export default Knowledge;
